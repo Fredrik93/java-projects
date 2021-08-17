1c900d33c9be2ac4a4fd732b0ace4486186ff92b
Fredrik93 <fredrikullman93@gmail.com>
2021-08-17 11:20:04 +0200

    Update from vscode

--- src/class_exercises/exerc_1.java
+++ src/class_exercises/exerc_1.java
@@ -3,7 +3,7 @@ package class_exercises;
 public class exerc_1 {
 
 	public static void main(String[] args) {
-		System.out.println("Exerc one from Eclipse");
+		System.out.println("Exerc one from vs code");
 	}
 
 }
