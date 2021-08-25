2e4f400e56cf0a5905de385a1b7753e1c0c57a6e
Fredrik93 <fredrikullman93@gmail.com>
2021-08-25 09:37:20 +0200

    Multiply two numbers from user input and square them

--- src/class_exercises/exerc_1.java
+++ src/class_exercises/exerc_1.java
@@ -1,9 +1,22 @@
 package class_exercises;
 
+import java.util.*;
+
 public class exerc_1 {
 
+	public static int square(int num, int num2) {
+		int mult = num * num2;
+		return mult * mult;
+	}
+
 	public static void main(String[] args) {
-		System.out.println("Exerc one from vs code");
+		Scanner scan = new Scanner(System.in);
+		System.out.println("Enter first number ");
+		int num1 = scan.nextInt();
+		System.out.println("And the second ");
+		int num2 = scan.nextInt();
+
+		System.out.println(square(num1, num2) + " is the num");
 	}
 
 }
