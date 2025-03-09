637c56703fc3e54c7e968f5e447107616baffa0a
Fredrik Ullman <42198444+Fredrik93@users.noreply.github.com>
2025-03-09 09:13:12 +0100

    Create SubArrays.java
    work in progress

---
+++ src/hackerrank/SubArrays.java
@@ -0,0 +1,90 @@
+import java.io.*;
+import java.util.*;
+import java.util.stream.Stream;
+
+public class Solution {
+
+    public static void main(String[] args) {
+        //read input 
+        Scanner sc = new Scanner(System.in);
+        int size = 0;
+        size = sc.nextInt();
+        int [] array = new int[size];
+        int index = 0;
+        int counter = 0;
+        System.out.println("Size: " + size);
+        while(sc.hasNextInt()){
+             //save to array 
+            array [index] = sc.nextInt();
+            index ++;
+        }
+       System.out.println(Arrays.toString(array));
+    
+       
+        //iterate once and find index of negative integers 
+        for(int i=0; i<size-1; i++){
+            if(i > 0){
+                //get the integer just before the negative integer
+                int previous = array[i-1];
+                int current = array[i];
+                int next = array[i+1];
+                //sum the integer (i-1), the actual negative integer (i) and the next (i+1) integer
+                int sum = previous + current + next;
+        //create counter that increments for each negative integer
+         if(current < 0){
+             counter++;
+            System.out.println("negative integer: " + current + ", counter is now: " + counter);
+        // if at least one integer is negative, itarate over the entire array and sum all integers and if negative, increment counter by 1
+        int sumOfAllElements = Arrays.stream(array).sum();
+        
+        System.out.println("total is " + sumOfAllElements + " incrementing..");
+        if(sumOfAllElements < 0){
+            counter++;
+        }
+        }                
+            System.out.println("prev: " + previous + " current: " + current + " next: " + next);
+            System.out.println("sum: " + sum);
+            if(sum < 0){
+                counter ++;
+                System.out.println("sum is negative, incrementing. counter is: " + counter) ;
+            }
+             
+            }
+            
+            else{
+                if(array[i] < 0){
+                    System.out.println("0th index item is negative: " + array[i]);
+                    counter++;
+                    System.out.println("0th index item is negative, incrementing: " + counter);
+                }
+           }
+        }
+           System.out.println("negative subarrays: " + counter);
+        
+        // if they are negative, increment counter +1
+        //print the counter 
+        
+    }
+}
+
+/**
+https://www.hackerrank.com/challenges/java-negative-subarray/problem?isFullScreen=true
+We define the following:
+
+A subarray of an -element array is an array composed from a contiguous block of the original array's elements. For example, if , then the subarrays are , , , , , and . Something like  would not be a subarray as it's not a contiguous subsection of the original array.
+The sum of an array is the total sum of its elements.
+An array's sum is negative if the total sum of its elements is negative.
+An array's sum is positive if the total sum of its elements is positive.
+Given an array of  integers, find and print its number of negative subarrays on a new line.
+
+Input Format
+
+The first line contains a single integer, , denoting the length of array .
+The second line contains  space-separated integers describing each respective element, , in array .
+
+Constraints
+
+Output Format
+
+Print the number of subarrays of  having negative sums.
+*/
